d94092e7ee11bcccb7a3ffd808d75c5259f632f4
yykyeong <yykyeong@naver.com>
2023-06-19 18:14:20 +0900

    헤더_검색바_스크립트

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter')
 const basketEl = basketStarterEl.querySelector('.basket')
 
@@ -17,10 +18,29 @@ window.addEventListener('click', function () {
   hideBasket()
 })
 
-
 function showBasket() {
   basketEl.classList.add('show')
 }
 function hideBasket() {
   basketEl.classList.remove('show')
+}
+
+// 검색바
+const headerEl = document.querySelector('header')
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed')
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
 }

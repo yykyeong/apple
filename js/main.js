59de4e0e9f067cfd5b0580e889521437db3cf418
yykyeong <yykyeong@naver.com>
2023-06-20 17:43:16 +0900

    compare 섹션

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
+import ipads from '../data/ipads.js'
+
 // 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter')
 const basketEl = basketStarterEl.querySelector('.basket')
 
-basketStarterEl.addEventListener('click',function(event) {
-  event.stopPropagation() 
+basketStarterEl.addEventListener('click', function (event) {
+  event.stopPropagation()
   if (basketEl.classList.contains('show')) {
     hideBasket()
   } else {
     showBasket()
   }
 })
-basketEl.addEventListener('click',function(event){
+basketEl.addEventListener('click', function (event) {
   event.stopPropagation() // 이벤트 버블링 정지! - 드롭다운 메뉴 영역을 클릭했을 때 메뉴가 사라지지 않아야 함.
 })
 
@@ -21,6 +23,7 @@ window.addEventListener('click', function () {
 function showBasket() {
   basketEl.classList.add('show')
 }
+
 function hideBasket() {
   basketEl.classList.remove('show')
 }
@@ -43,23 +46,24 @@ function showSearch() {
   headerEl.classList.add('searching')
   document.documentElement.classList.add('fixed')
   headerMenuEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / headerMenuEls.length  + 's'
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
   })
   searchDelayEls.forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / searchDelayEls.length  + 's'
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
   })
   setTimeout(function () {
     searchInputEl.focus()
   }, 600)
 }
+
 function hideSearch() {
   headerEl.classList.remove('searching')
   document.documentElement.classList.remove('fixed')
   headerMenuEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / headerMenuEls.length  + 's'
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
   })
   searchDelayEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / searchDelayEls.length  + 's'
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
   })
   searchDelayEls.reverse() // 원래상태로 다시 뒤집어주기
   searchInputEl.value = '' // 인풋요소 초기화
@@ -87,13 +91,42 @@ const video = document.querySelector('.stage video')
 const playBtn = document.querySelector('.stage .controller--play')
 const pauseBtn = document.querySelector('.stage .controller--pause')
 
-playBtn.addEventListener('click',function(){
+playBtn.addEventListener('click', function () {
   video.play()
   playBtn.classList.add('hide')
   pauseBtn.classList.remove('hide')
 })
-pauseBtn.addEventListener('click',function(){
+pauseBtn.addEventListener('click', function () {
   video.pause()
   playBtn.classList.remove('hide')
   pauseBtn.classList.add('hide')
+})
+
+// 당신에게 맞는 iPad는? 랜더링
+const itemsEl = document.querySelector('section.compare .items')
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement('div')
+  itemEl.classList.add('item')
+
+  let colorList = ''
+  ipad.colors.forEach(function (color) {
+    colorList += `<li style="background-color: ${color};"></li>`
+  })
+
+  itemEl.innerHTML = /* html */ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString('en-US')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  </div>
+  `
+
+  itemsEl.append(itemEl)
 })

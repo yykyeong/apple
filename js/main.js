38edb673ec541d3854ed6a62bd66cf171f58d987
yykyeong <yykyeong@naver.com>
2023-06-20 15:17:04 +0900

    video 스타일 및 제어

--- js/main.js
+++ js/main.js
@@ -80,3 +80,20 @@ const io = new IntersectionObserver(entries => {
 const infoEls = document.querySelectorAll('.info')
 // 관찰 시작!
 infoEls.forEach(el => io.observe(el))
+
+
+// 비디오 재생
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.stage .controller--play')
+const pauseBtn = document.querySelector('.stage .controller--pause')
+
+playBtn.addEventListener('click',function(){
+  video.play()
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+})
+pauseBtn.addEventListener('click',function(){
+  video.pause()
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+})
